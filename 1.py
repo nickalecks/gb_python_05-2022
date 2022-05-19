448b7158837fdca23bbb79fc6efe84ce6d645af2
daleko7 <105823193+daleko7@users.noreply.github.com>
2022-05-19 03:26:18 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+a=0
+b=0
+a=input('Enter number ')
+b=int(input('Enter second number '))
+print (type(a),a)
+print(type(b),b)
---
+++ 3.py
@@ -0,0 +1,4 @@
+n=0
+n=int(input('Введите число '))
+n=int(f'{n}')+int(f'{n}{n}')+int(f'{n}{n}{n}')
+print(n)
