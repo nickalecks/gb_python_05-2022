0e90aab3bdb7bd5f905f5103f1ba72ecd7a21481
naem <naem@reg87.ru>
2022-05-29 21:13:07 +0300

    adding a completed task to lesson 3

---
+++ lesson.03/task_3.py
@@ -0,0 +1,19 @@
+"""
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+"""
+
+
+def my_func(a, b, c):
+    sequence = [a, b, c]
+    summary_sec = []
+    max_1 = max(sequence)
+    summary_sec.append(max_1)
+    sequence.remove(max_1)
+    max_2 = max(sequence)
+    summary_sec.append(max_2)
+    print(sum(summary_sec))
+
+
+my_func(int(input("Введите первое число: ")), int(input("Введите второе число: ")),
+        int(input("Введите третье число: ")))
